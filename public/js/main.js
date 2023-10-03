b8c7898fce2cc5cd6706cab38835ffdb6aafa63d
ducha2112 <ducha2112@yandex.ru>
2023-10-03 16:57:27 +0300

    add js,img

---
+++ public/js/main.js
@@ -0,0 +1,34 @@
+
+function showText() {
+        $('#text').html('<h2>Ресурсы по разработке сайта</h2><ul><li class="resource"><a href="#">Termux для андроид</a></li><li class="resource"><a href="#">Редактор кода для андроид</a></li><li class="resource"><a href="#">NodeJS</a></li><li class="resource"><a href="#">Фреймворк Express.js</a></li></ul>');
+    }
+
+let body = document.querySelector('body');
+    $('#in').html('<h2 onclick="showText()">Hello, Dolly!</h2><div id="text"></div>');
+    
+const arr = ['node.jpg','express.jpg','code.jpg','nodejs.png','maxres.jpg'];
+function iterator(array){
+    let i = 0;
+    return {
+       next: function(){
+           if(i==arr.length) i=0;
+           return i < array.length ?  'img/'+ array[i++] :'';
+       }
+    }
+}
+let back ='';
+let text = iterator(arr);
+
+setInterval(function() {
+    back = text.next();
+        let styles = {
+            'backgroundImage':"linear-gradient(rgba(0, 0, 255, 0.8), rgba(199, 21, 133, 0.8)),url("+back+")",
+            'backgroundSize':'contain',
+            'backgroundPosition':'80% 90%'
+}
+    $('body').css(styles).animate({
+        'backgroundPositionX':'30%',
+        'backgroundPositionY':'40%',
+    },6000);
+
+}, 10000);
